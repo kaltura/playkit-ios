4b83a3159bfa192cc767981ce2d1d8ab1b3676e3
Noam Tamim <noam.tamim@kaltura.com>
2016-11-23 14:25:07 +0200

    Removed Pods from Example app.

--- Example/Pods/Target Support Files/PlayKit/PlayKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PlayKit : NSObject
-@end
-@implementation PodsDummy_PlayKit
-@end
--- Example/Pods/Target Support Files/PlayKit/PlayKit-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double PlayKitVersionNumber;
-FOUNDATION_EXPORT const unsigned char PlayKitVersionString[];
-
