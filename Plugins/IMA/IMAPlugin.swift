40319a56479a4ffd75bd3b56ca32547158a55525
Vadim Kononov <Vadim.Kononov@kaltura.com>
2016-11-28 16:15:21 +0200

    always notify ima plugin events to its delegate and post on message bus

--- Plugins/IMA/IMAPlugin.swift
+++ Plugins/IMA/IMAPlugin.swift
@@ -307,6 +307,11 @@ public class IMAPlugin: NSObject, AVPictureInPictureControllerDelegate, PlayerDe
         }
     }
 
+    private func notify(event: AdEvents) {
+        self.delegate?.adsPlugin(self, didReceive: event)
+        self.messageBus?.post(event)
+    }
+    
     private func destroyManager() {
         self.manager?.destroy()
         self.manager = nil
@@ -372,9 +377,7 @@ public class IMAPlugin: NSObject, AVPictureInPictureControllerDelegate, PlayerDe
         }
         
         let event = converted.init()
-        self.delegate?.adsPlugin(self, didReceive: event)
-        
-        messageBus?.post(event)
+        self.notify(event: event)
     }
     
     public func adsManager(_ adsManager: IMAAdsManager!, didReceive error: IMAAdError!) {
@@ -383,13 +386,13 @@ public class IMAPlugin: NSObject, AVPictureInPictureControllerDelegate, PlayerDe
     }
     
     public func adsManagerDidRequestContentPause(_ adsManager: IMAAdsManager!) {
-        self.delegate?.adsPlugin(self, didReceive: AdEvents.adDidRequestPause())
+        self.notify(event: AdEvents.adDidRequestPause())
         self.isAdPlayback = true
     }
     
     public func adsManagerDidRequestContentResume(_ adsManager: IMAAdsManager!) {
         self.showLoadingView(false, alpha: 0)
-        self.delegate?.adsPlugin(self, didReceive: AdEvents.adDidRequestResume())
+        self.notify(event: AdEvents.adDidRequestResume())
         self.isAdPlayback = false
     }
     
@@ -397,7 +400,7 @@ public class IMAPlugin: NSObject, AVPictureInPictureControllerDelegate, PlayerDe
         var data = [String : TimeInterval]()
         data["mediaTime"] = mediaTime
         data["totalTime"] = totalTime
-        self.delegate?.adsPlugin(self, didReceive: AdEvents.adDidProgressToTime(mediaTime: mediaTime, totalTime: totalTime))
+        self.notify(event: AdEvents.adDidProgressToTime(mediaTime: mediaTime, totalTime: totalTime))
     }
     
     // MARK: AVPictureInPictureControllerDelegate
@@ -435,22 +438,22 @@ public class IMAPlugin: NSObject, AVPictureInPictureControllerDelegate, PlayerDe
     // MARK: IMAWebOpenerDelegate
     
     public func webOpenerWillOpenExternalBrowser(_ webOpener: NSObject) {
-        self.delegate?.adsPlugin(self, didReceive: AdEvents.adWebOpenerWillOpenExternalBrowser(webOpener: webOpener))
+        self.notify(event: AdEvents.adWebOpenerWillOpenExternalBrowser(webOpener: webOpener))
     }
     
     public func webOpenerWillOpen(inAppBrowser webOpener: NSObject!) {
-        self.delegate?.adsPlugin(self, didReceive: AdEvents.adWebOpenerWillOpenInAppBrowser(webOpener: webOpener))
+        self.notify(event: AdEvents.adWebOpenerWillOpenInAppBrowser(webOpener: webOpener))
     }
     
     public func webOpenerDidOpen(inAppBrowser webOpener: NSObject!) {
-        self.delegate?.adsPlugin(self, didReceive: AdEvents.adWebOpenerDidOpenInAppBrowser(webOpener: webOpener))
+        self.notify(event: AdEvents.adWebOpenerDidOpenInAppBrowser(webOpener: webOpener))
     }
     
     public func webOpenerWillClose(inAppBrowser webOpener: NSObject!) {
-        self.delegate?.adsPlugin(self, didReceive: AdEvents.adWebOpenerWillCloseInAppBrowser(webOpener: webOpener))
+        self.notify(event: AdEvents.adWebOpenerWillCloseInAppBrowser(webOpener: webOpener))
     }
     
     public func webOpenerDidClose(inAppBrowser webOpener: NSObject!) {
-        self.delegate?.adsPlugin(self, didReceive: AdEvents.adWebOpenerDidCloseInAppBrowser(webOpener: webOpener))
+        self.notify(event: AdEvents.adWebOpenerDidCloseInAppBrowser(webOpener: webOpener))
     }
 }
