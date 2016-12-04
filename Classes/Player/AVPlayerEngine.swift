efc9ba017de9b1962f35da729b67acc4fbf01d9f
Eliza Sapir <eliza.sapir@kaltura.com>
2016-12-05 00:03:14 +0200

    code optimizations

--- Classes/Player/AVPlayerEngine.swift
+++ Classes/Player/AVPlayerEngine.swift
@@ -267,62 +267,94 @@ class AVPlayerEngine : AVPlayer {
             super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
             return
         }
+        
+        guard let keyPath = keyPath else {
+            return
+        }
+        
+        PKLog.trace("keyPath:: \(keyPath)")
 
         var event: PKEvent? = nil
         
-        if keyPath == #keyPath(currentItem.isPlaybackLikelyToKeepUp) {
-            if let item = self.currentItem {
-                let newState = PlayerState.ready
-                self.postStateChange(newState: newState, oldState: self.currentState)
-                self.currentState = newState
-            }
-        } else if keyPath == #keyPath(currentItem.isPlaybackBufferEmpty) {
-            if let item = self.currentItem {
-                let newState = PlayerState.idle
-                self.postStateChange(newState: newState, oldState: self.currentState)
-                self.currentState = newState
-            }
-        } else if keyPath == #keyPath(currentItem.duration) {
+        switch keyPath {
+        case #keyPath(currentItem.playbackLikelyToKeepUp):
+            self.handleLikelyToKeepUp()
+        case #keyPath(currentItem.playbackBufferEmpty):
+            self.handleBufferEmptyChange()
+        case #keyPath(currentItem.duration):
             event = PlayerEvents.durationChange(duration: CMTimeGetSeconds((self.currentItem?.duration)!))
-        } else if keyPath == #keyPath(rate) {
+        case #keyPath(rate):
             if rate == 1.0 {
                 nonObservablePropertiesUpdateTimer.resume()
             } else {
                 event = PlayerEvents.pause()
             }
-        } else if keyPath == #keyPath(currentItem.status) {
-            if currentItem?.status == .readyToPlay {
-                self.setupNonObservablePropertiesUpdateTimer()
-                
-                let newState = PlayerState.ready
-                self.postEvent(event: PlayerEvents.loadedMetadata())
-                self.postStateChange(newState: newState, oldState: self.currentState)
-                self.currentState = newState
-                
-                event = PlayerEvents.canPlay()
-            } else if currentItem?.status == .failed {
-                let newState = PlayerState.error
-                self.postStateChange(newState: newState, oldState: self.currentState)
-                self.currentState = newState
-                
-                event = PlayerEvents.error()
-            }
-        } else if keyPath == #keyPath(currentItem) {
+        case #keyPath(currentItem.status):
+            event = self.handleStatusChange()
+        case #keyPath(currentItem):
+            self.handleItemChange()
+            
+        default:
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+
+        self.postEvent(event: event)
+    }
+    
+    private func handleLikelyToKeepUp() {
+        if let item = self.currentItem {
+            let newState = PlayerState.ready
+            self.postStateChange(newState: newState, oldState: self.currentState)
+            self.currentState = newState
+        }
+    }
+    
+    private func handleBufferEmptyChange() {
+        if let item = self.currentItem {
             let newState = PlayerState.idle
             self.postStateChange(newState: newState, oldState: self.currentState)
             self.currentState = newState
         }
-
-        if let currentEvent: PKEvent = event {
-           self.postEvent(event: currentEvent)
-        }
     }
     
-    private func postEvent(event: PKEvent) {
-        PKLog.trace("onEvent:: \(event)")
+    private func handleStatusChange() -> PKEvent? {
+        var event: PKEvent? = nil
         
-        if let block = onEventBlock {
-            block(event)
+        if currentItem?.status == .readyToPlay {
+            self.setupNonObservablePropertiesUpdateTimer()
+            
+            let newState = PlayerState.ready
+            self.postEvent(event: PlayerEvents.loadedMetadata())
+            self.postStateChange(newState: newState, oldState: self.currentState)
+            self.currentState = newState
+            
+            event = PlayerEvents.canPlay()
+        } else if currentItem?.status == .failed {
+            let newState = PlayerState.error
+            self.postStateChange(newState: newState, oldState: self.currentState)
+            self.currentState = newState
+            
+            event = PlayerEvents.error()
+        }
+        
+        return event
+    }
+    
+    private func handleItemChange() {
+        let newState = PlayerState.idle
+        self.postStateChange(newState: newState, oldState: self.currentState)
+        self.currentState = newState
+    }
+    
+    private func postEvent(event: PKEvent?) {
+        if let currentEvent: PKEvent = event {
+            PKLog.trace("onEvent:: \(currentEvent)")
+            
+            if let block = onEventBlock {
+                block(currentEvent)
+            }
+        } else {
+            PKLog.error("event is empty:: \(event)")
         }
     }
     
