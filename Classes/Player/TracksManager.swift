9f449c175aebee6c3ebe75269fcbfbb6280b5003
Eliza Sapir <eliza.sapir@kaltura.com>
2016-12-11 18:03:27 +0200

    handle text track with no diff between sbtl and captions

--- Classes/Player/TracksManager.swift
+++ Classes/Player/TracksManager.swift
@@ -10,7 +10,7 @@ import Foundation
 import AVFoundation
 
 public class TracksManager {
-    let textTypeKey: String = "sbtl"
+    let audioTypeKey: String = "soun"
     private var audioTracks: [Track]?
     private var textTracks: [Track]?
     
@@ -41,10 +41,10 @@ public class TracksManager {
         let type: String = idArr[0]
         let index: Int = Int(idArr[1])!
         
-        if type == textTypeKey {
-            self.selectTextTrack(item: item, trackId: index)
+        if type == audioTypeKey {
+            self.selectAudioTrack(item: item, index: index)
         } else {
-            self.selectAudioTrack(item: item, trackId: index)
+            self.selectTextTrack(item: item, type: type, index: index)
         }
     }
     
@@ -70,19 +70,19 @@ public class TracksManager {
         }
     }
     
-    private func selectAudioTrack(item: AVPlayerItem, trackId: Int) {
+    private func selectAudioTrack(item: AVPlayerItem, index: Int) {
         PKLog.trace("selectAudioTrack")
         
         let audioSelectionGroup = item.asset.mediaSelectionGroup(forMediaCharacteristic: AVMediaCharacteristicAudible)
         audioSelectionGroup?.options.forEach { (option) in
-            var index = 0
+            var trackIndex = 0
             
-            if index == trackId {
+            if trackIndex == index {
                 PKLog.trace("option:: \(option)")
                 item.select(option, in: audioSelectionGroup!)
             }
 
-            index = index + 1
+            trackIndex += 1
         }
     }
     
@@ -106,45 +106,26 @@ public class TracksManager {
             var trackId = "\(option.mediaType):\(String(index))"
             let track = Track(id: trackId, title: option.displayName, language: option.extendedLanguageTag)
             
-            if option.hasMediaCharacteristic(AVMediaCharacteristicContainsOnlyForcedSubtitles) {
-                //subtitles
-                subtitles.append(track)
-            } else  {
-                //closed captions
-                captions.append(track)
-            }
-        }
-        
-        // Only one text track will be chosen
-        if subtitles.count > 0 {
-            self.textTracks = subtitles
-        } else if captions.count > 0 {
-            self.textTracks = captions
-        } else {
-            self.textTracks = nil
+            self.textTracks?.append(track)
         }
     }
     
-    private func selectTextTrack(item: AVPlayerItem, trackId: Int) {
+    private func selectTextTrack(item: AVPlayerItem, type: String, index: Int) {
         PKLog.trace("selectTextTrack")
         
         let textSelectionGroup = item.asset.mediaSelectionGroup(forMediaCharacteristic: AVMediaCharacteristicLegible)
         textSelectionGroup?.options.forEach { (option) in
-            var index = 0
+            var trackIndex = 0
             
-            if index == trackId {
+            if trackIndex == index {
                 PKLog.trace("option:: \(option)")
-                // Prefer Subtitles
-                if option.hasMediaCharacteristic(AVMediaCharacteristicContainsOnlyForcedSubtitles) {
-                    //subtitles
-                    item.select(option, in: textSelectionGroup!)
-                } else  {
-                    //closed captions
+                
+                if option.mediaType == type {
                     item.select(option, in: textSelectionGroup!)
                 }
             }
             
-            index = index + 1
+            trackIndex += 1
         }
     }
     
