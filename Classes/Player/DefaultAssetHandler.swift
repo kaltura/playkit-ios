a1a12d92b3fe24cb9a257e7323506a29d97ba967
Noam Tamim <noam.tamim@kaltura.com>
2016-12-13 22:08:25 +0200

    Implemented source selection in AssetBuilder.

--- Classes/Player/DefaultAssetHandler.swift
+++ Classes/Player/DefaultAssetHandler.swift
@@ -17,6 +17,33 @@ class DefaultAssetHandler: AssetHandler {
     required init() {
         
     }
+    
+    static let sourceFilter = { (_ src: MediaSource) -> Bool in
+        
+        guard let ext = src.contentUrl?.pathExtension else {
+            return false
+        }
+        
+        // mp4 is always supported
+        if ext == "mp4" {
+            return true
+        }
+        
+        // The only other option is HLS
+        if ext != "m3u8" {
+            return false
+        }
+        
+        // DRM is not supported on simulators
+        if src.drmData != nil && TARGET_OS_SIMULATOR != 0 {
+            return false
+        }
+        
+        // Source is HLS, with or without DRM.
+        return true
+    }
+
+    
     func buildAsset(mediaSource: MediaSource, readyCallback: (Error?, AVAsset?)->Void) {
 
         guard let contentUrl = mediaSource.contentUrl else {
