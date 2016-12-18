b9af7070a29027cb5471438c64f63b38bd6f67c6
Rivka Schwartz <Rivka.Peleg@kaltura.com>
2016-12-18 20:06:47 +0200

    Adding @discardableResult Adding logs And reordering

--- Addons/GoogleCast/GoogleCastAddon.swift
+++ Addons/GoogleCast/GoogleCastAddon.swift
@@ -10,6 +10,7 @@ import UIKit
 import GoogleCast
 
 
+
 public class KPGoogleCastAddon: NSObject {
 
     public class MediaInfoBuilder: NSObject {
@@ -25,86 +26,67 @@ public class KPGoogleCastAddon: NSObject {
         var metaData: GCKMediaMetadata?
         
         
+        @discardableResult
         public func set(contentId: String?) -> Self{
             self.contentId = contentId
             return self
         }
         
+        @discardableResult
         public func set(adTagURL: String?) -> Self {
             self.adTagURL = adTagURL
             return self
         }
         
+        @discardableResult
         public func set(webPlayerURL: String?) -> Self {
             self.webPlayerURL = webPlayerURL
             return self
         }
         
+        @discardableResult
         public func set(ks:String?) -> Self {
             self.ks = ks
             return self
             
         }
         
+        @discardableResult
         public func set(partnerID: String?) -> Self {
             self.partnerID = partnerID
             return self
         }
         
+        @discardableResult
         public func set(uiconfID: String?) -> Self {
             self.uiconfID = uiconfID
             return self
         }
         
+        @discardableResult
         public func set(initObject: [String:Any]?) -> Self {
             self.initObject = initObject
             return self
         }
         
+        @discardableResult
         public func set(format: String?) -> Self {
             self.format = format
             return self
         }
         
+        @discardableResult
         public func set(metaData: GCKMediaMetadata?) -> Self{
             self.metaData = metaData
             return self
         }
         
-        func flashVars() -> [String: Any]{
-            let proxyData = self.proxyData()
-            let doubleClickPlugin = self.doubleClickPlugin()
-            var flashVars: [String:Any] = ["proxyData":proxyData,
-                                           "doubleClick":doubleClickPlugin]
-            return flashVars
-        }
-        
-        func proxyData() -> [String:Any] {
-            let entryId = self.contentId
-            let flavorAssets = ["filters":["include":["Format":[self.format]]]]
-            let baseEntry  = ["vars":["isTrailer":" false"]]
-            let proxyData : [String : Any] = ["flavorassets":flavorAssets,
-                                              "baseentry":baseEntry,
-                                              "ks":self.ks,
-                                              "MediaID":entryId,
-                                              "iMediaID":entryId,
-                                              "initObj":self.initObject]
-            return proxyData
-        }
-        
-        func doubleClickPlugin() -> [String:Any] {
-            
-            let adTagURL = self.adTagURL
-            let plugin = ["plugin":"true",
-                    "adTagUrl":adTagURL]
-            return plugin
-        }
-
         
         
         public func build() -> GCKMediaInformation? {
             
             guard let entryId = self.contentId else {
+                PKLog.error("entryId must be sent")
                 return nil
             }
             
@@ -113,6 +95,13 @@ public class KPGoogleCastAddon: NSObject {
             embedConfig["publisherID"] = self.partnerID
             embedConfig["uiconfID"] = self.uiconfID
             embedConfig["entryID"] = entryId
+            
+            let flashVars = self.flashVars()
+            guard JSONSerialization.isValidJSONObject(flashVars) else {
+                PKLog.error("flash vars is not valid json")
+                return nil
+            }
+            embedConfig["flashVars"] = flashVars
             let customData: [String:Any] = ["embedConfig":embedConfig]
             let mediaInfo: GCKMediaInformation = GCKMediaInformation(contentID: entryId,
                                                                      streamType: GCKMediaStreamType.unknown,
@@ -121,6 +110,61 @@ public class KPGoogleCastAddon: NSObject {
                                                                      streamDuration: 0,
                                                                      customData: customData)
             return mediaInfo
+        }
+        
+        
+        // MARK - Build flash vars json:
+        private func flashVars() -> [String: Any]{
+            
+            var flashVars = [String:Any]()
+            if let proxyData = self.proxyData() {
+                PKLog.warning("proxyData is empty")
+                flashVars["proxyData"] = proxyData
+            }
+            
+            if let doubleClickPlugin = self.doubleClickPlugin() {
+                PKLog.warning("doubleClickPlugin is empty")
+                flashVars["doubleClick"] = doubleClickPlugin
+            }
+            
+            return flashVars
+        }
+        
+        private func proxyData() -> [String:Any]? {
+            
+            guard
+                let entryID = self.contentId,
+                let format = self.format
+                 else {
+                return nil
+            }
+            
+            let flavorAssets = ["filters":["include":["Format":[format]]]]
+            let baseEntry  = ["vars":["isTrailer":" false"]]
+            var proxyData : [String : Any] = ["flavorassets":flavorAssets,
+                                              "baseentry":baseEntry,
+                                              "MediaID":entryID,
+                                              "iMediaID":entryID]
+            
+            if let ks = self.ks {
+              proxyData["ks"] = ks
+            }
+            
+            if let initObject = self.initObject {
+              proxyData["initObj"] = initObject
+            }
+            return proxyData
+        }
+        
+        private func doubleClickPlugin() -> [String:Any]? {
+            
+            guard let adTagURL = self.adTagURL else {
+                return nil
+            }
+            
+            let plugin = ["plugin":"true",
+                    "adTagUrl":"\(adTagURL)"]
+            return plugin
         }
     }
     
