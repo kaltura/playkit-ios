3082e9b5d5a7c68e3ed10ce37d3a45b17c7240ec
odedk14 <kleinoded@gmail.com>
2017-01-01 13:02:37 +0200

    Fem 876 kaltura live stats (#53)
    * Added KalturaLiveStats Plugin.
    
    * KalturaLiveStats
    
    * Added default URL.
    
    * Added Start Time.
    
    * Added observer to bitrate change.

---
+++ Plugins/KalturaLiveStats/LiveStatsService.swift
@@ -0,0 +1,54 @@
+//
+//  LiveStatsService
+//  Pods
+//
+//  Created by Oded Klein on 07/12/2016.
+//
+//
+
+import UIKit
+import SwiftyJSON
+
+internal class LiveStatsService {
+
+    internal static func sendLiveStatsEvent(baseURL: String,
+                                            partnerId: String,
+                                            eventType: Int,
+                                            eventIndex: Int,
+                                            bufferTime: Int32,
+                                            bitrate: Int32,
+                                            sessionId: String,
+                                            startTime: Int32,
+                                            entryId: String,
+                                            isLive: Bool,
+                                            clientVer: String,
+                                            deliveryType: String) -> RequestBuilder? {
+        
+        if let request: RequestBuilder = RequestBuilder(url: baseURL) {
+            request
+                .setParam(key: "clientTag", value: "kwidget:v\(clientVer)")
+                .setParam(key: "service", value: "liveStats")
+                .setParam(key: "apiVersion", value: "3.1")
+                .setParam(key: "expiry", value: "86400")
+                .setParam(key: "format", value: "1")
+                .setParam(key: "ignoreNull", value: "1")
+                .setParam(key: "action", value: "collect")
+                .setParam(key: "event:eventType", value: "\(eventType)")
+                .setParam(key: "event:partnerId", value: partnerId)
+                .setParam(key: "event:sessionId", value: sessionId)
+                .setParam(key: "event:eventIndex", value: "\(eventIndex)")
+                .setParam(key: "event:bufferTime", value: "\(bufferTime)")
+                .setParam(key: "event:bitrate", value: "\(bitrate)")
+                .setParam(key: "event:isLive", value: "\(isLive)")
+                .setParam(key: "event:startTime", value: "\(startTime)")
+                .setParam(key: "event:entryId", value: entryId)
+                .setParam(key: "event:deliveryType", value: deliveryType)
+            
+                .set(method: "GET")
+            return request
+        }else{
+            return nil
+        }
+    }
+
+}
