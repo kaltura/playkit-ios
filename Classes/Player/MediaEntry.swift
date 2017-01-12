f6c66e0ae8986f8cffd214dccca9b2389be0bc96
Noam Tamim <noam.tamim@kaltura.com>
2017-01-12 23:02:59 +0200

    Some fixes and cleanups in source selection and related.
    Before this change, mp4 would be selected instead of HLS if it came before in the sources list.

--- Classes/Player/MediaEntry.swift
+++ Classes/Player/MediaEntry.swift
@@ -120,11 +120,12 @@ public class MediaSource: NSObject {
         self.init(id, contentUrl: nil)
     }
     
-    public init(_ id: String, contentUrl: URL?, mimeType: String? = nil, drmData: [DRMData]? = nil) {
+    public init(_ id: String, contentUrl: URL?, mimeType: String? = nil, drmData: [DRMData]? = nil, sourceType: SourceType? = nil) {
         self.id = id
         self.contentUrl = contentUrl
         self.mimeType = mimeType
         self.drmData = drmData
+        self.sourceType = sourceType
     }
     
     public init(json: Any) {
