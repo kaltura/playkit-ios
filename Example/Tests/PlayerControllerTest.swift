0262bb08434bf7fb5bc6aeb43ea3d09600050083
Gal Orlanczyk <gal.orlanczyk@kaltura.com>
2017-01-18 14:24:30 +0200

    Added unit-tests for bug fix, checking 2 scenarios: 1. Make sure pause event isn’t received after ended. 2. Make sure after seeking pause event is received as usual.

--- Example/Tests/PlayerControllerTest.swift
+++ Example/Tests/PlayerControllerTest.swift
@@ -9,12 +9,13 @@
 import XCTest
 import PlayKit
 import SwiftyJSON
+import CoreMedia
 
 //Unit test to check the player controller
 //If somthing is break here we should raise a red flag - since this is our public API
 class PlayerControllerTest: XCTestCase {
-    var player : Player!
     
+    var player : Player!
     
     override func setUp() {
         super.setUp()
@@ -35,9 +36,7 @@ class PlayerControllerTest: XCTestCase {
         let media = MediaEntry(json: entry)
         config.set(mediaEntry: media)
         self.player = PlayKitManager.sharedInstance.loadPlayer(config:config)
-        
-        
-}
+    }
     
     override func tearDown() {
         super.tearDown()
@@ -52,8 +51,6 @@ class PlayerControllerTest: XCTestCase {
             } else {
                 XCTFail()
             }
-            
-            
         }
         waitForExpectations(timeout: 10.0) { (_) -> Void in}
     }
@@ -67,8 +64,6 @@ class PlayerControllerTest: XCTestCase {
             } else {
                 XCTFail()
             }
-            
-            
         }
         self.player.pause()
         waitForExpectations(timeout: 10.0) { (_) -> Void in}
@@ -95,7 +90,83 @@ class PlayerControllerTest: XCTestCase {
                 }
             }
         }
-       
         waitForExpectations(timeout: 10.0) { (_) -> Void in}
+    }
+    
+    /// Test a guard mechanism that prevents receiving pause events after ended event.
+    ///
+    /// ## The Flow:
+    /// 1. play the video.
+    /// 2. seek to 2 seconds before the end.
+    /// 3. on ended event pause the player.
+    ///
+    /// **expected result:** shouldn't receive the pause event and expectation should be fullfilled.
+    func testEnded() {
+        let asyncExpectation = expectation(description: "ended event")
+        var isEnded = false
+        var isFirstPlay = true
+        
+        player.play()
+        player.addObserver(self, events: [PlayerEvents.ended.self]) { info in
+            print("ended")
+            isEnded = true
+            self.player.pause()
+        }
+        self.player.addObserver(self, events: [PlayerEvents.playing.self, PlayerEvents.pause.self]) { info in
+            if info is PlayerEvents.playing && isFirstPlay && self.player.isPlaying {
+                isFirstPlay = false
+                // seek to end - 1 second
+                self.player.seek(to: CMTimeMake(Int64(self.player.duration - 2), 1))
+            }
+            // should not fire play/pause after ended
+            if isEnded {
+                XCTFail()
+            }
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 9) {
+            asyncExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    /// Test to make sure pause/play events are received after ended + seeked event.
+    ///
+    /// ## The Flow:
+    /// 1. play the video.
+    /// 2. seek to 2 seconds before the end.
+    /// 3. on ended event seek again to 2 seconds before the end.
+    /// 4. play again
+    /// 
+    /// **expected result:** receive play event after ended + seeked
+    func testAnalyticsEndedSeekedPlayed() {
+        let asyncExpectation = expectation(description: "ended -> seek -> play events")
+        var isEnded = false
+        var isFirstPlay = true
+        var isSeekedAfterEnded = false
+        
+        player.play()
+        player.addObserver(self, events: [PlayerEvents.ended.self]) { info in
+            print("ended")
+            isEnded = true
+            self.player.seek(to: CMTimeMake(Int64(self.player.duration - 2), 1))
+        }
+        player.addObserver(self, events: [PlayerEvents.playing.self, PlayerEvents.pause.self]) { info in
+            if info is PlayerEvents.playing && isFirstPlay && self.player.isPlaying {
+                isFirstPlay = false
+                // seek to end - 2 second
+                self.player.seek(to: CMTimeMake(Int64(self.player.duration - 2), 1))
+            }
+            // should fire play/pause after ended + seeked
+            if isSeekedAfterEnded {
+                asyncExpectation.fulfill()
+            }
+        }
+        player.addObserver(self, events: [PlayerEvents.seeked.self]) { info in
+            if isEnded {
+                isSeekedAfterEnded = true
+                self.player.play()
+            }
+        }
+        waitForExpectations(timeout: 20, handler: nil)
     }
 }
