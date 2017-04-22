cadd6aed5ac1f63872a132c9c4169798d713609b
Rivka Schwartz <Rivka.Peleg@kaltura.com>
2017-04-22 22:58:30 +0300

    Adding guard to avoid warning

--- Classes/Providers/OVP/Parsers/OVPObjectMapper.swift
+++ Classes/Providers/OVP/Parsers/OVPObjectMapper.swift
@@ -17,7 +17,11 @@ class OVPObjectMapper: NSObject {
     
     static func classByJsonObject(json:Any?) -> OVPBaseObject.Type? {
         
-        let jsonObject = JSON(json)
+        guard let js = json else {
+            return nil
+        }
+        
+        let jsonObject = JSON(js)
         let className = jsonObject[classNameKey].string
         if let name = className{
             switch name {
