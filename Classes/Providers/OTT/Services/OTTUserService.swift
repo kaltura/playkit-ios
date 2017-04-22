2bffd4a1111a47ac6dc8d7e875b177b3a9c65e0e
Rivka Schwartz <Rivka.Peleg@kaltura.com>
2017-04-22 22:58:01 +0300

    deleting unused API's login & refresh session & logout

--- Classes/Providers/OTT/Services/OTTUserService.swift
+++ Classes/Providers/OTT/Services/OTTUserService.swift
@@ -12,59 +12,16 @@ import KalturaNetKit
 
 class OTTUserService: NSObject {
 
-    internal static func login(baseURL: String, partnerId: Int64, username: String, password: String, udid: String? = nil) -> KalturaRequestBuilder? {
-
-        if let request = KalturaRequestBuilder(url: baseURL, service: "ottUser", action: "login") {
-            request
-                .setBody(key: "username", value: JSON(username))
-                .setBody(key: "password", value: JSON(password))
-                .setBody(key: "partnerId", value: JSON(NSNumber.init(value: partnerId)))
-
-            if let deviceId = udid {
-                request.setBody(key: "udid", value: JSON(udid))
-            }
-            return request
-        }
-
-        return nil
-    }
-
-    internal static func refreshSession(baseURL: String, refreshToken: String, ks: String, udid: String? = nil) -> KalturaRequestBuilder? {
-        if let request = KalturaRequestBuilder(url: baseURL, service: "ottUser", action: "refreshSession") {
-            request
-                .setBody(key: "refreshToken", value: JSON(refreshToken))
-                .setBody(key: "ks", value: JSON(ks))
-            if let deviceId = udid {
-                request.setBody(key: "udid", value: JSON(udid))
-            }
-            return request
-        }
-        return nil
-    }
-
     internal static func anonymousLogin(baseURL: String, partnerId: Int64, udid: String? = nil) -> KalturaRequestBuilder? {
         if let request = KalturaRequestBuilder(url: baseURL, service: "ottUser", action: "anonymousLogin") {
             request.setBody(key: "partnerId", value: JSON(NSNumber.init(value: partnerId)))
 
             if let deviceId = udid {
-                request.setBody(key: "udid", value: JSON(udid))
+                request.setBody(key: "udid", value: JSON(deviceId))
             }
             return request
         }
         return nil
     }
 
-    internal static func logout(baseURL: String, partnerId: Int64, ks: String, udid: String? = nil) -> KalturaRequestBuilder? {
-        if let request = KalturaRequestBuilder(url: baseURL, service: "ottUser", action: "logout") {
-            request.setBody(key: "ks", value: JSON(ks))
-            request.setBody(key: "partnerId", value: JSON(NSNumber.init(value: partnerId)))
-
-            if let deviceId = udid {
-                request.setBody(key: "udid", value: JSON(udid))
-            }
-
-            return request
-        }
-        return nil
-    }
 }
