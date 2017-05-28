81e24c7fbaaef828793ade3e92af74597e4f2e7a
Gal Orlanczyk <gal-orlanczyk@users.noreply.github.com>
2017-05-28 12:12:04 +0300

    Tracks (#172)
    * small improvement to tracks

--- Classes/Player/Track.swift
+++ Classes/Player/Track.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 @objc public class Track: NSObject {
-    @objc public var id: String?
-    @objc public var title: String?
+    @objc public var id: String
+    @objc public var title: String
     @objc public var language: String?
     
-    init(id: String?, title: String?, language: String?) {
+    init(id: String, title: String, language: String?) {
         PKLog.debug("init:: id:\(String(describing: id)) title:\(String(describing: title)) language: \(String(describing: language))")
         
         self.id = id
