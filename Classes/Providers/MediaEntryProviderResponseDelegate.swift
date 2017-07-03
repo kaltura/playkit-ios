8e0c60780f1a9e4c23b3aed4dc23534ad0024c70
Rivka Schwartz <Rivka.Peleg@kaltura.com>
2017-07-03 18:14:51 +0300

    Adding a way to listen to the response of the get play back context and other requests are being sent by the mediaEntryProvider

---
+++ Classes/Providers/MediaEntryProviderResponseDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  MediaEntryProviderResponseDelegate.swift
+//  Pods
+//
+//  Created by Rivka Peleg on 03/07/2017.
+//
+//
+
+import Foundation
+
+//This protocol provides a way to use the response data of the requests are being sent by MediaEntryProvider.
+//For example the response of getPlaybackContext, or additional meta data.
+
+@objc public protocol MediaEntryProviderResponseDelegate {
+    
+    @objc func providerGotResponse(sender:MediaEntryProvider?, response: [String:Any]?) -> Void
+
+}
