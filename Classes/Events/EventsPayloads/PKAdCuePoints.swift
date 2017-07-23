4443adfdb94481da097ca5e753e5e5866eb1547a
Gal Orlanczyk <gal.orlanczyk@kaltura.com>
2017-07-23 11:40:20 +0300

    Added user agent to AVURLAsset

--- Classes/Events/EventsPayloads/PKAdCuePoints.swift
+++ Classes/Events/EventsPayloads/PKAdCuePoints.swift
@@ -14,7 +14,7 @@ import Foundation
     
     @objc public private(set) var cuePoints: [TimeInterval]
     
-    public init(cuePoints: [TimeInterval]) {
+    @objc public init(cuePoints: [TimeInterval]) {
         self.cuePoints = cuePoints.sorted() // makes sure array is sorted
     }
     
