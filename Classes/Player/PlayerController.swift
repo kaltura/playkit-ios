7b5e5cdd0cf6bc7a89c8214ffda63887d18c9d0a
Gal Orlanczyk <gal.orlanczyk@kaltura.com>
2017-07-26 09:10:05 +0300

    PR issues fix

--- Classes/Player/PlayerController.swift
+++ Classes/Player/PlayerController.swift
@@ -103,6 +103,8 @@ class PlayerController: NSObject, Player, PlayerSettings {
     
     func setMedia(from mediaConfig: MediaConfig) {
         self.mediaConfig = mediaConfig
+        // create new media session uuid
+        self.mediaSessionUUID = UUID()
         
         // get the preferred media source and post source selected event
         guard let (preferredMediaSource, handlerType) = AssetBuilder.getPreferredMediaSource(from: mediaConfig.mediaEntry) else { return }
@@ -215,13 +217,11 @@ extension PlayerController {
     /// Updates the request adapter if one exists
     fileprivate func updateRequestAdapter(in mediaSource: inout MediaSource) {
         // configure media sources content request adapter if request adapter exists
-        if let _ = self.contentRequestAdapter {
-            // create new media session uuid
-            self.mediaSessionUUID = UUID()
+        if let adapter = self.contentRequestAdapter {
             // update the request adapter with the updated session id
-            self.contentRequestAdapter!.updateRequestAdapter(with: self)
+            adapter.updateRequestAdapter(with: self)
             // configure media source with the adapter
-            mediaSource.contentRequestAdapter = self.contentRequestAdapter!
+            mediaSource.contentRequestAdapter = adapter
         }
     }
 }
