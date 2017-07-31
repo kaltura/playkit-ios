ba4a7be4ed64b5ee3114d72c90eff65884c20f41
Eliza Sapir <eliza.sapir@kaltura.com>
2017-07-31 18:27:57 +0300

    remove additional header

--- Classes/Backend/Base/Model/KalturaBaseObject.swift
+++ Classes/Backend/Base/Model/KalturaBaseObject.swift
@@ -8,14 +8,6 @@
 // https://www.gnu.org/licenses/agpl-3.0.html
 // ===================================================================================================
 
-//
-//  KalturaBaseObject.swift
-//  Pods
-//
-//  Created by Rivka Peleg on 05/07/2017.
-//
-//
-
 import Foundation
 
 protocol KalturaBaseObject {
