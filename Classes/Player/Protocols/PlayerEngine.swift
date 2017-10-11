4691d7b0a67b9394de60204c22cadb7587d1e55d
kaltura-fe-ci <kaltura-fe-ci.bot@kaltura.com>
2017-10-11 08:36:20 +0300

    Add copyright banner

--- Classes/Player/Protocols/PlayerEngine.swift
+++ Classes/Player/Protocols/PlayerEngine.swift
@@ -1,6 +1,16 @@
 // ===================================================================================================
 // Copyright (C) 2017 Kaltura Inc.
 //
+// Licensed under the AGPLv3 license, unless a different license for a 
+// particular library is specified in the applicable library path.
+//
+// You may obtain a copy of the License at
+// https://www.gnu.org/licenses/agpl-3.0.html
+// ===================================================================================================
+
+// ===================================================================================================
+// Copyright (C) 2017 Kaltura Inc.
+//
 // Licensed under the AGPLv3 license, unless a different license for a
 // particular library is specified in the applicable library path.
 //
