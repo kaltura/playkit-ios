2b4060a2f520abf4f1e87026ed48e1a664dd57e6
Nilit Danan <nilit.danan@kaltura.com>
2018-05-14 14:24:47 +0300

    Fix for currentPosition returning a negative value.

--- Classes/Player/AVPlayerEngine/AVPlayerEngine.swift
+++ Classes/Player/AVPlayerEngine/AVPlayerEngine.swift
@@ -66,11 +66,18 @@ public class AVPlayerEngine: AVPlayer {
     /// because the current segment wasn't yet added to the seekable time ranges.
     public var currentPosition: TimeInterval {
         get {
-            print("Nilit AVPlayerEngine \(self.currentTime()) - \(self.rangeStart)")
             let position = self.currentTime() - self.rangeStart
+            
             PKLog.trace("get currentPosition: \(position)")
-            let time = CMTimeGetSeconds(position)
-            // time could be NaN in some rare cases make sure we don't return NaN and return 0 otherwise.
+            var time = CMTimeGetSeconds(position)
+            
+            // In some cases the video will start playing with negative current time.
+            // self.currentTime() returns a large negative value
+            if time < 0.0 {
+                time = 0
+            }
+            
+            // Time could be NaN in some rare cases make sure we don't return NaN and return 0 otherwise.
             return time.isNaN ? 0 : time
         }
         set {
