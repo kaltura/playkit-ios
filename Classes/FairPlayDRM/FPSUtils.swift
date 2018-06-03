4d8e6b225baebad3c67182118fa5dc256b295054
kaltura-fe-ci <kaltura-fe-ci.bot@kaltura.com>
2018-06-03 17:36:20 +0300

    Add copyright banner

--- Classes/FairPlayDRM/FPSUtils.swift
+++ Classes/FairPlayDRM/FPSUtils.swift
@@ -1,3 +1,13 @@
+// ===================================================================================================
+// Copyright (C) 2018 Kaltura Inc.
+//
+// Licensed under the AGPLv3 license, unless a different license for a 
+// particular library is specified in the applicable library path.
+//
+// You may obtain a copy of the License at
+// https://www.gnu.org/licenses/agpl-3.0.html
+// ===================================================================================================
+
 //
 //  FPSUtils.swift
 //  PlayKit
