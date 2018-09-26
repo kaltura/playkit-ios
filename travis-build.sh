b91e337635c583d4d29b6cf5ba7fd102256c817c
Noam Tamim <noam.tamim@kaltura.com>
2018-09-26 23:57:12 +0300

    Make travis-build.sh more explicit

--- travis-build.sh
+++ travis-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# If we're building a proper tag (v1.2.3), push to cocoapods.
 if [[ $TRAVIS_TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]
 then
   # push to cocoapods, this also lints before
@@ -14,7 +15,9 @@ echo Executing pod $CMD
 
 # Travis aborts the build if it doesn't get output for a long while.
 FLAG=$(mktemp)
-pod $CMD --allow-warnings && rm $FLAG & while [ -f $FLAG ]; do sleep 5; echo . ; done;
+keepAlive &
+pod $CMD --allow-warnings
+rm $FLAG
 
 login() {
 cat << EOF > ~/.netrc
@@ -24,4 +27,12 @@ machine trunk.cocoapods.org
 EOF
 
 chmod 0600 ~/.netrc
+}
+
+keepAlive() {
+  while [ -f $FLAG ]
+  do 
+    sleep 5
+    echo .
+  done
 }
