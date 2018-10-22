4be862f0cfc7ad9e82bd89dda83061ba7d19e8cd
Noam Tamim <noam.tamim@kaltura.com>
2018-10-22 22:54:45 +0300

    TestApp: simple build of the pod
    Now we have 3 types of build:
    1. Quick build: just `pod install` and build the TestApp. This is triggered by pushes and pull requests.
    2. Lint: `pod lib lint`; only happens in cron builds
    3. Push: `pod trunk push`; only when building tags.

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -e -o pipefail
 
 # Login to cocoapods trunk.
 login() {
@@ -22,20 +22,37 @@ keepAlive() {
   done
 }
 
-FLAG=$(mktemp)
-
-keepAlive $FLAG &
-
-# If we're building a release tag (v1.2.3), push to cocoapods, else lint.
-if [[ $TRAVIS_TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]
-then
-  echo "Pushing to Trunk"
+trunkPush() {
+  FLAG=$(mktemp)
+  keepAlive $FLAG &
   login
   pod trunk push --allow-warnings
-else
-  echo "Linting the pod"
+  rm $FLAG  # stop keepAlive
+}
+
+justBuild() {
+  cd TestApp
+  pod install
+  xcodebuild build -workspace TestApp.xcworkspace -scheme TestApp -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO | xcpretty
+}
+
+libLint() {
+  FLAG=$(mktemp)
+  keepAlive $FLAG &
   pod lib lint --allow-warnings
+  rm $FLAG  # stop keepAlive
+}
+
+
+pod repo update
+
+# If we're building a release tag (v1.2.3) push to cocoapods, else build TestApp.
+if [[ $TRAVIS_TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]
+then
+  trunkPush
+elif [ $TRAVIS_EVENT_TYPE == cron ]
+  libLint
+else
+  justBuild
 fi
 
-rm $FLAG  # stop keepAlive
-echo "Cocoapods done"
