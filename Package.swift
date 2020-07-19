cfa32d74099006ac5e69ec95597380c40b8cedd2
Noam Tamim <noam.tamim@kaltura.com>
2020-07-19 23:00:35 +0300

    Set `platforms` in Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "PlayKit",
+    platforms: [
+        .iOS(.v9), .tvOS(.v9)
+    ],
     products: [
         .library(
             name: "PlayKit",
@@ -12,7 +15,7 @@ let package = Package(
     dependencies: [
     .package(name: "KalturaNetKit", url: "https://github.com/kaltura/netkit-ios.git", .branch("spm")),
     .package(name: "PlayKitUtils", url: "https://github.com/kaltura/playkit-ios-utils.git", .branch("spm")),
-    .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.1"),
+    .package(url: "https://github.com/noamtamim/XCGLogger.git", .branch("spm")),
     .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.3.0")
     ],
     targets: [
