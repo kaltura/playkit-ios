1fdc4265e35d1a9e59b832e70d07984fc3b93a0b
Nilit Danan <nilit.danan@kaltura.com>
2020-09-15 12:09:35 +0300

    Updated to pod install --repo-update because it fails to run pod repo update separately and the new pods are not fetched.

--- travis-build.sh
+++ travis-build.sh
@@ -8,6 +8,6 @@ else
   # Else just build the test app
   curl https://kaltura.github.io/fe-tools/ios/pod-app/create.sh | bash -s - "pod 'PlayKit', :path => '..'"
   cd HelloPod
-  pod install
+  pod install --repo-update
   xcodebuild -workspace HelloPod.xcworkspace -scheme HelloPod -sdk iphonesimulator
 fi
