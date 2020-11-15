bea1ae7148745b62b74bbfde73f6a42e704d3fc9
Gilad Nadav <gilad.nadav@kaltura.com>
2020-11-15 21:18:59 +0200

    add 3 new params to PlaybackInfo event
        /// The average bitrate of video track if it is unmuxed
        @objc public let averageVideoBitrate: Double
        /// The average bitrate of audio track. This is not available if audio is muxed with video.
        @objc public let averageAudioBitrate: Double
        /// The URI of the playback item
        @objc public let uri: String?

--- Classes/Events/EventsPayloads/PKPlaybackInfo.swift
+++ Classes/Events/EventsPayloads/PKPlaybackInfo.swift
@@ -20,11 +20,22 @@ import AVFoundation
     @objc public let indicatedBitrate: Double
     /// The throughput of the playback (download speed)
     @objc public let observedBitrate: Double
-    
-    init(bitrate: Double, indicatedBitrate: Double, observedBitrate: Double) {
+    /// The average bitrate of video track if it is unmuxed
+    @objc public let averageVideoBitrate: Double
+    /// The average bitrate of audio track. This is not available if audio is muxed with video.
+    @objc public let averageAudioBitrate: Double
+    /// The URI of the playback item
+    @objc public let uri: String?
+
+        
+    init(bitrate: Double, indicatedBitrate: Double, observedBitrate: Double, averageVideoBitrate: Double, averageAudioBitrate: Double, uri: String?) {
         self.bitrate = bitrate
         self.indicatedBitrate = indicatedBitrate
         self.observedBitrate = observedBitrate
+        self.averageVideoBitrate = averageVideoBitrate
+        self.averageAudioBitrate = averageAudioBitrate
+        self.uri = uri
+
     }
     
     convenience init(logEvent: AVPlayerItemAccessLogEvent) {
@@ -38,6 +49,26 @@ import AVFoundation
         }
         let indicatedBitrate = logEvent.indicatedBitrate
         let observedBitrate = logEvent.observedBitrate
-        self.init(bitrate: bitrate, indicatedBitrate: indicatedBitrate, observedBitrate: observedBitrate)
+        let averageAudioBitrate: Double
+        let averageVideoBitrate: Double
+        if #available(iOS 10.0, *) {
+            if (logEvent.averageVideoBitrate > 0) {
+                averageVideoBitrate = logEvent.averageVideoBitrate
+            } else {
+                averageVideoBitrate = bitrate
+            }
+            if (logEvent.averageAudioBitrate > 0) {
+                averageAudioBitrate = logEvent.averageAudioBitrate
+            } else {
+                averageAudioBitrate = -1
+            }
+        } else {
+            averageVideoBitrate = bitrate
+            averageAudioBitrate = -1
+        }
+        let uri:String?
+        uri = logEvent.uri ?? ""
+    
+        self.init(bitrate: bitrate, indicatedBitrate: indicatedBitrate, observedBitrate: observedBitrate, averageVideoBitrate: averageVideoBitrate, averageAudioBitrate: averageAudioBitrate, uri: uri)
     }
 }
