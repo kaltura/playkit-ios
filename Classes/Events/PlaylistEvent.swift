f6dcb62c9e7b4d8534c7a5c24e3d89a30dcbf654
Sergey Chausov <chouzoff@gmail.com>
2021-10-31 18:24:43 +0200

    FEC-11505 Playlist events, work in progress

---
+++ Classes/Events/PlaylistEvent.swift
@@ -0,0 +1,69 @@
+// ===================================================================================================
+// Copyright (C) 2021 Kaltura Inc.
+//
+// Licensed under the AGPLv3 license, unless a different license for a
+// particular library is specified in the applicable library path.
+//
+// You may obtain a copy of the License at
+// https://www.gnu.org/licenses/agpl-3.0.html
+// ===================================================================================================
+//
+//  PlaylistEvent.swift
+//  PlayKit
+//
+//  Created by Sergii Chausov on 27.10.2021.
+//
+
+import Foundation
+
+@objc public class PlaylistEvent: PKEvent {
+    
+    @objc public static let allEventTypes: [PlaylistEvent.Type] = [
+        playListLoaded, playListStarted, playListEnded, playlistCountDownStart, playlistCountDownEnd, playlistLoopStateChanged, playlistLoopStateChanged, playlistAutoContinueStateChanged, playListError, playListLoadMediaError
+    ]
+    
+    @objc public static let playListLoaded: PlaylistEvent.Type = PlayListLoaded.self
+    @objc public static let playListStarted: PlaylistEvent.Type = PlayListStarted.self // ?
+    @objc public static let playListEnded: PlaylistEvent.Type = PlayListEnded.self
+    @objc public static let playlistCountDownStart: PlaylistEvent.Type = PlaylistCountDownStart.self // ?
+    @objc public static let playlistCountDownEnd: PlaylistEvent.Type = PlaylistCountDownEnd.self // ?
+    @objc public static let playlistLoopStateChanged: PlaylistEvent.Type = PlaylistLoopStateChanged.self
+    @objc public static let playlistAutoContinueStateChanged: PlaylistEvent.Type = PlaylistAutoContinueStateChanged.self
+    @objc public static let playListError: PlaylistEvent.Type = PlayListError.self
+    @objc public static let playListLoadMediaError: PlaylistEvent.Type = PlayListLoadMediaError.self
+    
+    public class PlayListLoaded: PlaylistEvent {}
+    
+    public class PlayListStarted: PlaylistEvent {}
+    
+    public class PlayListEnded: PlaylistEvent {}
+    
+    public class PlaylistCountDownStart: PlaylistEvent {}
+    
+    public class PlaylistCountDownEnd: PlaylistEvent {}
+    
+    public class PlaylistLoopStateChanged: PlaylistEvent {}
+    
+    public class PlaylistAutoContinueStateChanged: PlaylistEvent {}
+    
+    public class PlayListError: PlaylistEvent {
+        public convenience init(nsError: NSError) {
+            self.init([EventDataKeys.error: nsError])
+        }
+        
+        public convenience init(error: PKError) {
+            self.init([EventDataKeys.error: error.asNSError])
+        }
+    }
+    
+    public class PlayListLoadMediaError: PlaylistEvent {
+        public convenience init(nsError: NSError) {
+            self.init([EventDataKeys.error: nsError])
+        }
+        
+        public convenience init(error: PKError) {
+            self.init([EventDataKeys.error: error.asNSError])
+        }
+    }
+    
+}
