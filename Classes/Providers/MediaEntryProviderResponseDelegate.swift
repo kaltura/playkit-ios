6d35576b1b8b1100b750f15733080b4ac2f4fbed
TL <tommylohz@gmail.com>
2022-01-16 13:33:46 +0800

    fix: protocol compiler warnings

--- Classes/Providers/MediaEntryProviderResponseDelegate.swift
+++ Classes/Providers/MediaEntryProviderResponseDelegate.swift
@@ -14,7 +14,7 @@ import KalturaNetKit
 //This protocol provides a way to use the response data of the requests are being sent by MediaEntryProvider.
 //For example the response of getPlaybackContext, or additional meta data.
 
-public protocol PKMediaEntryProviderResponseDelegate: class {
+public protocol PKMediaEntryProviderResponseDelegate: AnyObject {
     
     func providerGotResponse(sender: MediaEntryProvider?, response: Response) -> Void
 
