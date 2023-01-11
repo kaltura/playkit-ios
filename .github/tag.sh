baf3ee86a891514e40d246fc2698895e00de5be8
Sergey Chausov <chouzoff@gmail.com>
2023-01-11 23:29:07 +0200

    Workflow updated

--- .github/tag.sh
+++ .github/tag.sh
@@ -23,6 +23,6 @@ POST_URL=https://api.github.com/repos/$GITHUB_REPOSITORY/git/refs
 curl $POST_URL -X POST -H "Content-Type: application/json" -H "authorization: Bearer $GITHUB_TOKEN" -d@post.json
 
 #Add current tag to job output
-run: echo "{tag}={$TARGET_TAG}" >> $GITHUB_OUTPUT
+run: echo "tag=${TARGET_TAG}" >> $GITHUB_OUTPUT
 
 echo "$NAME release tag added and it is ready for CocoaPods distribution, upcoming version is going to be $TARGET_TAG"
